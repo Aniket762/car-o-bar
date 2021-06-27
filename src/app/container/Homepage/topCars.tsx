50fcd922ce02dfdcd6822edd2e8532fd7780ade2
Aniket762 <aniketindian8@gmail.com>
2021-06-27 14:58:44 +0530

    carousel responsive

--- src/app/container/Homepage/topCars.tsx
+++ src/app/container/Homepage/topCars.tsx
@@ -5,6 +5,8 @@ import { ICar } from "../../../typings/car";
 import { Car } from "../../components/car";
 import Carousel, { Dots, slidesToShowPlugin } from "@brainhubeu/react-carousel";
 import "@brainhubeu/react-carousel/lib/style.css";
+import { useMediaQuery } from "react-responsive";
+import { SCREENS } from "../../components/responsive";
 
 const TopCarsContainer = styled.div`
   ${tw`
@@ -46,58 +48,74 @@ const CarsContainer = styled.div`
 export function TopCars()
 {
     const [current, setCurrent] = useState(0);
+    const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
 
-  const testCar: ICar = {
-    name: "Audi S3 Car",
-    mileage: "10k",
-    thumbnailSrc:
-      "https://cdn.jdpower.com/Models/640x480/2017-Audi-S3-PremiumPlus.jpg",
-    dailyPrice: 70,
-    monthlyPrice: 1600,
-    gearType: "Auto",
-    gas: "Petrol",
-  };
-
-  const testCar2: ICar = {
-    name: "HONDA cITY 5 Seater Car",
-    mileage: "20k",
-    thumbnailSrc:
-      "https://shinewiki.com/wp-content/uploads/2019/11/honda-city.jpg",
-    dailyPrice: 50,
-    monthlyPrice: 1500,
-    gearType: "Auto",
-    gas: "Petrol",
+    const testCar: ICar = {
+        name: "Audi S3 Car",
+        mileage: "10k",
+        thumbnailSrc:
+            "https://cdn.jdpower.com/Models/640x480/2017-Audi-S3-PremiumPlus.jpg",
+        dailyPrice: 70,
+        monthlyPrice: 1600,
+        gearType: "Auto",
+        gas: "Petrol",
     };
+
+    const testCar2: ICar = {
+        name: "HONDA cITY 5 Seater Car",
+        mileage: "20k",
+        thumbnailSrc:
+            "https://shinewiki.com/wp-content/uploads/2019/11/honda-city.jpg",
+        dailyPrice: 50,
+        monthlyPrice: 1500,
+        gearType: "Auto",
+        gas: "Petrol",
+    };
+
+    const cars = [< Car { ...testCar } />,< Car { ...testCar2 } />,< Car { ...testCar } />,< Car { ...testCar2 } />]
+    const numberOfDots = isMobile ? cars.length : Math.ceil(cars.length / 3);
     
     return (
         <TopCarsContainer>
             <Title>Explore Top Cars</Title>
             <CarsContainer>
-
-                <Carousel
-                    value={current}
-                    OnChange={setCurrent}
-                    slides={[
-                        <Car {...testCar} />,
-                        <Car {...testCar2} />,
-                        <Car {...testCar} />,
-                        <Car {...testCar2} />
-                    ]}
-                    plugins={[
-                        {
-                            resolve: slidesToShowPlugin,
-                            options: {
-                                numberOfSlides:3,
-                            }
-                        }
-                    ]}
-                />
-            <Dots value={current} onChange={setCurrent} number={4} />
-{/* 
-            <Car {...testCar} />
-            <Car {...testCar} />
-            <Car {...testCar2} /> */}
-
+            <Carousel
+            value={current}
+            onChange={setCurrent}
+            slides={cars}
+            plugins={[
+              "clickToChange",
+              {
+                resolve: slidesToShowPlugin,
+                options: {
+                  numberOfSlides: 3,
+                },
+              },
+            ]}
+            breakpoints={{
+              640: {
+                plugins: [
+                  {
+                    resolve: slidesToShowPlugin,
+                    options: {
+                      numberOfSlides: 1,
+                    },
+                  },
+                ],
+              },
+              900: {
+                plugins: [
+                  {
+                    resolve: slidesToShowPlugin,
+                    options: {
+                      numberOfSlides: 2,
+                    },
+                  },
+                ],
+              },
+            }}
+          />
+            <Dots value={current} onChange={setCurrent} number={numberOfDots} />
             </CarsContainer>
         </TopCarsContainer>
     )
